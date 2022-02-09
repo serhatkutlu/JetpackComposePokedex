c67b622b4e54d8bce45baae9dd6861316d6a04b9
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-02-09 16:13:47 +0300

    initial commit

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
@@ -3,11 +3,22 @@ package com.plcoding.jetpackcomposepokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,8 +26,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePokedexTheme {
-
+                test()
             }
         }
+    }
+
+    @Preview
+    @Composable
+    fun test(){
+        Box(modifier = Modifier
+            .background(Color.Blue)
+            .fillMaxSize()){
+            Text(text = "TEST", textAlign = TextAlign.Center, fontSize = 40.sp, modifier = Modifier.align(Alignment.Center))
+        }
+
     }
 }
