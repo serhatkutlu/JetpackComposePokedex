f886b39b874a874c9ecb6f51af505853c8197a8c
serhatkutlu <84921644+serhatkutlu@users.noreply.github.com>
2022-02-10 14:22:23 +0300

    NavHost Created

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
@@ -6,27 +6,57 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+
+import androidx.navigation.compose.rememberNavController
+import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePokedexTheme {
-                test()
+                val navController= rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemon_list_screen" ){
+                    composable("pokemon_list_screen"){
+
+                    }
+                    composable("pokemon_detail_screen/{dominantColor}/{pokemonName}",
+                    arguments = listOf(
+                        navArgument("dominantColor"){
+                            type= NavType.IntType
+                        },
+                        navArgument("pokemonName"){
+                            type= NavType.StringType
+                        }
+                    )){
+                        val dominantColor=remember{
+                            val color=it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) }?: Color.White
+                        }
+                        val pokemonName= remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+
+                    }
+                }
             }
         }
     }
